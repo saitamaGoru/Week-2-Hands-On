029f205a554b44dce991d4f636d9ee520e6ec86e
pgrewa26 <300961555@cencol.ca>
2022-09-15 13:19:02 -0400

    added another package

--- src/wk2session2ex2/Account.java
+++ src/wk2session2ex2/Account.java
@@ -2,5 +2,32 @@ package wk2session2ex2;
 
 public class Account 
 {
+	private double balance;
+	private String name;
 	
+	public double getBalance()
+	{
+		return balance;
+	}
+	
+	public void setBalance(double balance)
+	{
+		this.balance=balance;
+	}
+	//constructor initializes name with parameter name
+	public Account(String name, double balance)
+	{
+		this.name=name;
+		this.balance=balance;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name=name;
+	}
 }
